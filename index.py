714f2e1a72c4d63ef8513ea756a5926e3f855d6b
kameniarthur <kameniarthur138@gmail.com>
2026-01-04 18:42:56 +0100

    add user input for YouTube URL and improve download options

--- index.py
+++ index.py
@@ -1,9 +1,10 @@
 import yt_dlp
 
-url = input("Entrez l'URL YouTube : ")
 ydl_opts = {
-    'format': 'bestvideo+bestaudio',
-    'outtmpl': '%(title)s.%(ext)s'
+    'format': 'bestvideo+bestaudio',  # Télécharge vidéo + audio
+    'merge_output_format': 'mp4',    # Fusionne en MP4
+    'outtmpl': '%(title)s.%(ext)s',
 }
+url = input("Entrez l'URL de la vidéo YouTube à télécharger : ")
 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
     ydl.download([url])   
